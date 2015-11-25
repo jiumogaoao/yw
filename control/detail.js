ccf10859503125ba0d1f27d0c6c3970ff0d3ba31
jiumogaoao <394127821@qq.com>
2015-11-26 07:35:18 +0800

    change
    change

---
+++ control/detail.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+;(function($,obj,config){
+	obj.control.set({
+		name:"detail",
+		par:[],
+		fn:function(data){
+			var tk="";
+			var objArry=[];
+			var typeArry=[];
+			var productArry=[];
+			var pomo=[];
+			function headLayput(){
+				obj.model.get("#head","headSimple","head",function(model){
+				/*model.set({
+				object:objArry,
+				type:0
+				});*/
+				model.reflash();
+				model.show();
+				});
+				}
+			function footLayout(){
+			obj.model.get("#foot","footSimple","foot",function(model){
+				model.show();
+				});
+				}
+			function mainLayout(){
+				obj.model.get("#main","rootNavSimple","rootNav",function(model){
+					model.show();
+					model.reflash();
+				});
+				obj.model.get("#main","detailTopSimple","detailTop",function(model){
+					model.show();
+					model.reflash();
+				});
+				}
+			
+			function getList(tka){
+				tk=tka;
+				var callbackcount=0;
+				var callbackfn=function(){
+					callbackcount++;
+					if(callbackcount===1){
+						headLayput();
+				footLayout();
+				mainLayout();
+						}
+					};
+					callbackfn();
+				}
+				getList("wdcfv");
+			//obj.api.tk(getList);
+			}
+		});
+	})($,app,config);
---
+++ model/detailTop.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+;(function($,obj,config){
+	obj.model.set({
+		name:"detailTop",
+		css:["detail_top"],
+		html:["detail_top"],
+		fn:function(){
+			
+			var source=this;
+			//init
+			source.init=function(){
+				
+				};
+			source.reflash=function(){
+				source.target.html(source.css[0]+source.html[0]);
+			};
+			//set
+			source.set=function(data){};
+			}
+		});
+	})($,app,config);
